250c16a4c5898ae9c52e93e4df32404e60f9ecc6
Sophie Buysse <70411966+sfbuysse@users.noreply.github.com>
2024-02-17 13:23:13 -0500

    Update BattleshipTournamentSeeds.R

--- BattleshipTournamentSeeds.R
+++ BattleshipTournamentSeeds.R
@@ -1,7 +1,6 @@
-# create seeds for the first battleship tournament
+##### create seeds for the first battleship tournament #####
 
 # Feb. 25th, 2023
-
 # single elimination
 
 set.seed(220)
@@ -14,4 +13,14 @@ names(seeds) <- c(1:10)
 
 print(seeds)
 
+##### create seeds for the second battleship tournament #####
 
+# Feb. 17th, 2024
+# single elimination
+
+set.seed(1998)
+participants <- c("sophie", "john", "brandon_b", "brandon_w", "miles", "riley",  "kim", "maya", "magie")
+seeds <- sample(participants, 9, replace = FALSE, prob = NULL)
+names(seeds) <- c(1:9)
+
+print(seeds)
